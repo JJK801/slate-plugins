de2fcac885e4a9cb4e82e5aeeaccabaa189b3528
Jérémy Jourdin <jjourdin18@gmail.com>
2020-11-25 19:07:40 +0100

    Clean code + toggle

--- packages/slate-plugins/src/elements/blockquote/transforms/toggleBlockquote.ts
+++ packages/slate-plugins/src/elements/blockquote/transforms/toggleBlockquote.ts
@@ -1,27 +1,19 @@
+import { isNodeTypeIn } from '../../../common';
 import { Editor, Transforms } from 'slate';
 import { setDefaults } from '../../../common/utils/setDefaults';
-import { ELEMENT_PARAGRAPH } from '../../paragraph';
 import { DEFAULTS_BLOCKQUOTE } from '../defaults';
 import { BlockquotePluginOptionsValues } from '../types';
 
 export const toggleBlockquote = (editor: Editor, options?: Record<"blockquote", BlockquotePluginOptionsValues>) => {
+  if (!editor.selection) return;
+
   const { blockquote } = setDefaults(options, DEFAULTS_BLOCKQUOTE);
 
-  Transforms.removeNodes(editor);
-  Transforms.insertNodes(
-    editor,
-    {
-      type: blockquote.type,
-      children: [
-        {
-          type: ELEMENT_PARAGRAPH,
-          children: [{ text: '' }],
-        }
-      ],
-    },
-    { 
-      match: (n) => Editor.isBlock(editor, n),
-      select: true
-    }
-  );
-};
+  const isActive = isNodeTypeIn(editor, blockquote.type);
+
+  if (!isActive) {
+    Transforms.wrapNodes(editor, { type: blockquote.type, children: [] });
+  } else {
+    Transforms.unwrapNodes(editor, {});
+  }
+}
